8cc0a7ae26a6e4da4c0756a45425c99f9ef30c1a
Achinda <achindamihiruk@gmail.com>
2023-08-03 09:41:02 +0530

    Coments updated

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,7 @@
 public class Demo1 {
     public static void main(String[] args) {
         byte myByte = -10;
-        char myChar = (char) myByte;           // sign extended
+        char myChar = (char) myByte;           // sign extended more accuracy 
         int myInt = myChar;
         short myShort = (short)myChar;
         byte myByte2 = (byte) myChar;
@@ -12,7 +12,7 @@ public class Demo1 {
 
         System.out.println("............................");
 
-        char myChar2 = (char) (myByte & 0xFF);  // sign not extended
+        char myChar2 = (char) (myByte & 0xFF);  // sign not extended less accuracy
         int myInt2 = myChar2;
         short myShort2 = (short) myChar2;
         byte myByte3 = (byte) myChar2;
